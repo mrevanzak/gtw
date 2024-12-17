7370e11c58672d6b7256a01dd10621b59f99162b
mrevanzak <mrevanzak@gmail.com>
2024-12-17 15:49:38 +0700

    feat: initial commit
    base template from
    https://github.com/huuquyet/expo-next-tamagui-monorepo
    hono template from https://github.com/w3cj/hono-open-api-starter

---
+++ apps/hono/src/env.ts
@@ -0,0 +1,39 @@
+import { config } from "dotenv";
+import { expand } from "dotenv-expand";
+import { z } from "zod";
+
+expand(config());
+
+const EnvSchema = z
+  .object({
+    NODE_ENV: z.string().default("development"),
+    PORT: z.coerce.number().default(8000),
+    LOG_LEVEL: z
+      .enum(["fatal", "error", "warn", "info", "debug", "trace", "silent"])
+      .default("info"),
+    DATABASE_URL: z.string().url(),
+    DATABASE_AUTH_TOKEN: z.string().optional(),
+  })
+  .superRefine((input, ctx) => {
+    if (input.NODE_ENV === "production" && !input.DATABASE_AUTH_TOKEN) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.invalid_type,
+        expected: "string",
+        received: "undefined",
+        path: ["DATABASE_AUTH_TOKEN"],
+        message: "Must be set when NODE_ENV is 'production'",
+      });
+    }
+  });
+
+export type env = z.infer<typeof EnvSchema>;
+
+const { data: env, error } = EnvSchema.safeParse(process.env);
+
+if (error) {
+  console.error("❌ Invalid env:");
+  console.error(JSON.stringify(error.flatten().fieldErrors, null, 2));
+  process.exit(1);
+}
+
+export default env!;
